cbc96528ece1e57663c0b526e19d66331ce999a6
Kaniel <kankoh98@gmail.com>
2023-12-17 21:18:21 +0800

    Backend set up

---
+++ pdf-summarizer-backend/src/main/java/com/personal/pdfsummarizer/PdfSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.personal.pdfsummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfSummarizerApplication.class, args);
+	}
+
+}
